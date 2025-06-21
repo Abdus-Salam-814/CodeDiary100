a9d030ea6d717ea28d2fbd79e72580f3ffbc459b
slam814 <abdussalam6111997@gmail.com>
2025-06-21 11:28:22 +0600

    tas-1 finesd here

---
+++ Day_2/task-1.js
@@ -0,0 +1,7 @@
+const myTaka = 100;
+
+if(myTaka >= 500 ){
+    console.log(`You Buy Burger ${myTaka} BDT. and You gataed free a Coke. `)
+}else{
+    console.log(`you Buy Burger ${myTaka} BDT. Can You Buy a coke Please pay 30tak`)
+}
