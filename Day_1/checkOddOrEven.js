cc65392f1c301b6b509eb53e59cd125ed9c6261d
slam814 <abdussalam6111997@gmail.com>
2025-06-26 09:13:01 +0600

    i added some file and function

---
+++ Day_1/checkOddOrEven.js
@@ -0,0 +1,15 @@
+function isEven(num){
+    return num % 2 === 0;
+}
+
+console.log(isEven(9))
+console.log(isEven(4))
+
+
+for(let i = 1; i<=100; i++){
+    if(i % 3 === 0){
+        console.log(i + "is Divisble by 3")
+    }
+}
+
+
