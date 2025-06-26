29e8c67b24197e93fa84ce8c57db877723e6499e
slam814 <abdussalam6111997@gmail.com>
2025-06-26 09:28:30 +0600

    if else condition added

---
+++ Day_2/if-else.js
@@ -0,0 +1,8 @@
+let score = 75;
+score = Math.ceil(Math.random() * 100);
+if(score > 50){
+    console.log(`You Passes the Exam. Your Score is ${score} `)
+}else{
+    console.log(`Sorry! Your fald ${score}`)
+}
+
