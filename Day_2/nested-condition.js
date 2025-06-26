d572e42db639f3505ef2fdadb19d296ac145884f
slam814 <abdussalam6111997@gmail.com>
2025-06-26 12:02:32 +0600

    i addeed some resources and code

---
+++ Day_2/nested-condition.js
@@ -0,0 +1,28 @@
+// let age = 18;
+// let hasId = false;
+
+
+
+// if(age >= 18){
+//     if(hasId){
+//         console.log("You Can Enter the Club");
+
+//     }else{
+//         console.log("ID Required To Enter.")
+//     }
+// }else{
+//     console.log("You are too Young to enter")
+// }
+
+
+
+
+let score = 85;
+
+if(score >= 90){
+    console.log("Grade A")
+}else{
+    if(score >= 80){
+        
+    }
+}
